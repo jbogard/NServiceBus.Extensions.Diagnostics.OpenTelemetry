67af927f891eb7bba40a347c3053747556427cce
Jimmy Bogard <jimmy.bogard@gmail.com>
2020-11-30 07:40:46 -0600

    Updating references

--- src/NServiceBus.Extensions.Diagnostics.OpenTelemetry/TracerProviderBuilderExtensions.cs
+++ src/NServiceBus.Extensions.Diagnostics.OpenTelemetry/TracerProviderBuilderExtensions.cs
@@ -1,24 +1,10 @@
 ﻿using System;
-using OpenTelemetry.Trace;
 
-namespace NServiceBus.Extensions.Diagnostics.OpenTelemetry
+namespace OpenTelemetry.Trace
 {
     public static class TracerProviderBuilderExtensions
     {
-        public static TracerProviderBuilder AddNServiceBusInstrumentation(this TracerProviderBuilder builder)
-            => builder.AddNServiceBusInstrumentation(null);
-
-        public static TracerProviderBuilder AddNServiceBusInstrumentation(this TracerProviderBuilder builder, Action<NServiceBusInstrumentationOptions> configureInstrumentationOptions)
-        {
-            var options = new NServiceBusInstrumentationOptions();
-
-            configureInstrumentationOptions ??= opt => { };
-            
-            configureInstrumentationOptions(options);
-            
-            return builder
-                .AddInstrumentation(t => new NServiceBusReceiveInstrumentation(t, options))
-                .AddInstrumentation(t => new NServiceBusSendInstrumentation(t, options));
-        }
+        public static TracerProviderBuilder AddNServiceBusInstrumentation(this TracerProviderBuilder builder) 
+            => builder.AddSource("NServiceBus.Extensions.Diagnostics");
     }
 }
